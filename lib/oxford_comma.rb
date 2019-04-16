37d347361627d435213ae424aa95ba500d596190
Elma Rahman <elmarahman635@gmail.com>
2019-04-16 19:36:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ def oxford_comma(array)
 if array.length == 1
   return "#{array[0]}"
 elsif array.length == 2
-  return array.join("and")
+  return array.join(" and ")
 else array.length >= 3
-array[-1] = "and #{array[-1]}" 
+array[-1] = "and #{array[-1]}"
   return array.join(",")
 
 end
