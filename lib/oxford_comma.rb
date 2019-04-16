a52a030dbb93075c88fbdf8875d45559c559a674
Elma Rahman <elmarahman635@gmail.com>
2019-04-16 19:32:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ if array.length == 1
   return "#{array[0]}"
 elsif array.length == 2
   return array.join("and")
-
 else array.length >= 3
-{array[-1]}" =
-
+{array[-1]}" = "and "#{array[-1]}"
 
 end
