f946801df75094788eae4c2667d04808aa227685
Elma Rahman <elmarahman635@gmail.com>
2019-04-16 18:46:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
 
+array = "kiwi"
+array << " and durian"
+array.join(" , ")
+
+#array.map{ |fruits| array << "kiwi"}
+
+#array << "kiwi"
+
+#array << "kiwi and durian"
+
+#array << "k"
+
 end
