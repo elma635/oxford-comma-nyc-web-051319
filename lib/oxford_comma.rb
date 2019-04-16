cd4581d07de673aedc36970ed24aab8cc5480c0c
Elma Rahman <elmarahman635@gmail.com>
2019-04-16 19:29:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,10 @@ def oxford_comma(array)
 
 if array.length == 1
   return "#{array[0]}"
-elsif array.length = 2
-  return
+elsif array.length == 2
+
+else array.length == 3
+{array[-1]}" = " and "
 
 
 end
